6eccf57fb1728294c1de01446cbca88b6a6eb770
prateekparallel <prateekparallel@gmail.com>
2018-09-12 21:53:09 +0100

    Adding PUT Controller class

---
+++ StudentRegistrationDemo2/Controllers/StudentUpdateController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using StudentRegistrationDemo2.Models;
+
+namespace StudentRegistrationDemo2.Controllers
+{
+    public class StudentUpdateController : ApiController
+    {
+        public String PutStudentRecord( Student stdn)
+        {
+            Console.WriteLine("In updateStudentRecord");
+            return StudentRegistration.getInstance().UpdateStudent(stdn);
+        }
+    }
+}
