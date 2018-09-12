e6e19c65a53cd346c5b66160c9c64fecbfe7b3bb
prateekparallel <prateekparallel@gmail.com>
2018-09-12 21:51:37 +0100

    Adding POST Controller

---
+++ StudentRegistrationDemo2/Controllers/StudentRegistrationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using StudentRegistrationDemo2.Models;
+
+namespace StudentRegistrationDemo2.Controllers
+{
+    public class StudentRegistrationController : ApiController
+    {
+        public StudentRegistrationReply registerStudent(Student studentregd)
+        {
+            Console.WriteLine("In registerStudent");
+            StudentRegistrationReply stdregreply = new StudentRegistrationReply();
+            StudentRegistration.getInstance().Add(studentregd);
+            stdregreply.Name = studentregd.Name;
+            stdregreply.Age = studentregd.Age;
+            stdregreply.RegistrationNumber = studentregd.RegistrationNumber;
+            stdregreply.RegistrationStatus = "Successful";
+
+            return stdregreply;
+        }
+
+    }
+}
