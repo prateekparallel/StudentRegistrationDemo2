f246883398260dad588e2a07cb2b05e92b39302f
prateekparallel <prateekparallel@gmail.com>
2018-09-12 21:55:13 +0100

    Adding DELETE Controller

---
+++ StudentRegistrationDemo2/Controllers/StudentDeleteController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using StudentRegistrationDemo2.Models;
+
+namespace StudentRegistrationDemo2.Controllers
+{
+    public class StudentDeleteController : ApiController
+    {
+        [Route("student/remove/{regdNum}")]
+
+        public String DeleteStudentRecord(String regdNum)
+        {
+            Console.WriteLine("In deleteStudentRecord");
+            return StudentRegistration.getInstance().Remove(regdNum);
+        }
+    }
+}
